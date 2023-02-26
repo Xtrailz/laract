90837abdfd1815ae878ca0532a09e43ce05bc5ca
Xtrailz <85149576+Xtrailz@users.noreply.github.com>
2023-02-26 19:50:40 +0700

    Add files via upload

---
+++ create.blade.php
@@ -0,0 +1,34 @@
+@extends('main.layout')
+@section('content')
+<center>
+    <h2>TAMBAH DATA KELAS</h2>
+    <form action="/kelas/store" method="POST">
+        @csrf
+        <table width="50%">
+            <tr>
+                <td class="bar">NAMA KELAS</td>
+                <td class="bar"><input type="text" name="nama_kelas"></td>
+            </tr>
+
+            <tr>
+                <td class="bar">JURUSAN</td>
+                <td class="bar">
+                    <select name="jurusan_id">
+                        <option value="" disabled selected hidden>PILIH JURUSAN</option>
+                    @foreach($jurusan as $jur)
+                    <option value="{{ $jur->id }}">{{ $jur->nama_jurusan }}</option>
+                    <!-- {{ $jur->id }}=sesuai dengan nomer id di tabel jurusan -->
+                @endforeach
+
+                </select>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <center><button type="submit" class="button-primary">SIMPAN</button></center>
+                </td>
+            </tr>
+        </table>
+    </form>
+</center>
+@endsection
